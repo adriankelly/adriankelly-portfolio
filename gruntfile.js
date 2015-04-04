9e5498358c6c7466cd82f87a38b16914d58a5640
Adrian Kelly <hello@adriankelly.net>
2015-04-04 17:27:48 +0100

    concat css files

--- gruntfile.js
+++ gruntfile.js
@@ -4,7 +4,12 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+
             // 2. Configuration for concatinating files goes here.
+
+
+
+// AUTOPREFIXER
         autoprefixer: {
 
           options: {
@@ -14,66 +19,75 @@ module.exports = function(grunt) {
           multiple_files: {
             expand: true,
             flatten: true,
-            src: 'css/*.css', // -> src/css/file1.css, src/css/file2.css
-            dest: 'css/ap/' // -> dest/css/file1.css, dest/css/file2.css
+            src: 'preprod/css/*.css', // -> src/css/file1.css, src/css/file2.css
+            dest: 'preprod/grunt/css/ap/' // -> dest/css/file1.css, dest/css/file2.css
           }
         },
 
-
-
-
-
-        cssmin: {
-          target: {
-            files: [{
-              expand: true,
-              cwd: 'css/ap',
-              src: ['*.css', '!*.min.css'],
-              dest: 'build/css/',
-              ext: '.min.css'
-            }]
-          }
-        },
+// IMAGE COMPRESSION
 
         imagemin: {
             dynamic: {
                 files: [{
                     expand: true,
-                    cwd: 'img/',
+                    cwd: 'preprod/img/',
                     src: ['**/*.{png,jpg,gif}'],
                     dest: 'build/img/'
                 }]
             }
         },
 
-        concat: {   
-          dist: {
-            src: [
-              'js/*.js', // All JS in the folder
+
+// CONCATENATE JS AND CSS
+
+    concat: {
+        css: {
+           src: [
+                 'preprod/grunt/css/ap/*'
+                ],
+            dest: 'preprod/grunt/css/combined.css'
+        },
+        js : {
+            src : [
+                'preprod/js/*.js'
             ],
-            dest: 'js/build/production.js',
-          }
+            dest : 'preprod/grunt/js/combined.js'
+        }
+    },
+
+
+// MINIFY CSS
+
+    cssmin : {
+            css:{
+                src: 'preprod/grunt/css/combined.css',
+                dest: 'build/css/style.min.css'
+            }
         },
 
-        uglify: {
-            build: {
-                src: 'js/build/production.js',
-                dest: 'build/js/script.min.js'
+
+// MINIFY JS
+
+    uglify: {
+        js: {
+            src: 'preprod/grunt/js/combined.js',
+            dest: 'build/js/script.min.js'
             }
-        }
+    }
 
 
 
-    });
+
+});
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['autoprefixer', 'cssmin', 'imagemin', 'concat', 'uglify']);
+    grunt.registerTask('default', ['autoprefixer', 'imagemin', 'concat:css', 'cssmin:css', 'concat:js', 'uglify']);
 
 };
