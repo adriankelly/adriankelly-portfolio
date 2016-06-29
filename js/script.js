16f0f1cd823ec338068aafed078a9c6b14b6879e
Adrian Kelly <hello@adriankelly.net>
2016-06-29 06:43:11 +0100

    Updated switch functionality

--- js/script.js
+++ js/script.js
@@ -1,48 +1,25 @@
 $(document).ready(function() {
+  // Validate contact form
+  $("#contact-form").validate();
+  // Remove internal link hash from URL
+  if (window.location.hash === "projects" || window.location.hash === "contact-me") {
+    loadContent(window.location.hash);
+  }
 
-    // Validate contact form
-    $("#contact-form").validate();
+  // Go to project website
+  $('.projects-container').click(function(e) {
+    window.location = $(this).children('div').children("a").attr("href");
+  })
 
-    // Remove internal link hash from URL
-    if (window.location.hash === "projects" || window.location.hash === "contact-me") {
-      loadContent(window.location.hash);
-    }
-
-
-    $('.header-nav').click(function(e) {
-
-        // remove classes from all
-        // $(".nav-links").removeClass("currentLink");
-        // add class to the one we clicked
-        $(e.target).toggleClass("currentLink");
-
-
-
-        // TweenMax.to('#projects', 0.75, {opacity: 0, ease:Quad.easeOut});
-        // TweenMax.to('#contact', 0.75, {opacity: 0, ease:Quad.easeOut});
-        var clicked = $(e.target.hash);
-        var clickedOp = clicked[0].style.opacity;
-
-        if (clickedOp === '1') {
-          TweenMax.to(e.target.hash, 0.75, { opacity: 0, ease: Quad.easeOut });
-        } else {
-          TweenMax.to(e.target.hash, 0.75, { opacity: 1, ease: Quad.easeOut });
-        }
-      })
-
-
-    $('.projects-container').click(function(e) {
-        // console.log($(this).children('div').children("a").attr("href"));
-        window.location = $(this).children('div').children("a").attr("href");
-      })
-
-    $('.down-arrow').click(function() {
-      $('#main-background-black').toggleClass('full-black');
-      if ($('#main-background-black').hasClass('full-black')) {
+  // Switch
+    $('#switch').click(function() {
+      $('#main-blackout').toggleClass('main-bg-blackout');
+      if ($('#main-blackout').hasClass('main-bg-blackout')) {
         TweenMax.to('#header-container', 0.5, { width: "18%" });
-        TweenMax.to('#header-name', 0.5, { fontSize: "2.5rem" });
+        TweenMax.to('#header-name', 0.5, { fontSize: "2.5rem", letterSpacing: "0.3rem" });
         TweenMax.to('#header-job-title', 0.5, { fontSize: "1.3rem", letterSpacing: "0rem", onStart: function() {
             TweenMax.to('.header-nav', 1, { opacity: 1 });
+            // $('a[href="#projects"]').addClass('currentLink');
           }
         });
         TweenMax.delayedCall(3, showProjects);
@@ -52,7 +29,7 @@ $(document).ready(function() {
         TweenMax.to('#header-job-title', 0.5, {
           fontSize: "3.5rem",
           onStart: function() {
-            TweenMax.to('.header-nav', 1, { opacity: 0 });
+            TweenMax.to('.header-nav', 0.5, { opacity: 0 });
           }
         });
         TweenMax.delayedCall(3, hideProjects);
@@ -79,13 +56,9 @@ function fadeUp() {
   tl.to('#down-arrow-glow', 0.5, { opacity: 1, yoyo: true, repeat: 8, repeatDelay: 0.5 }, 2);
   tl.to('#main-container', 0.75, {
     opacity: 1,
-    ease: Quad.easeOut,
-    onStart: function() {
-      $('a[href="#projects"]').addClass('currentLink');
-    }
+    ease: Quad.easeOut
   }, 0.2);
   tl.to('#header-container', 0.75, { opacity: 1, left: 0, ease: Quad.easeOut }, 0.5);
-  tl.to('#projects', 2, { opacity: 1, ease: Quad.easeOut }, 1.1);
 
   return tl;
 
