97f534e582c5e6f1e2d1aba69b101e1da185d057
Adrian Kelly <hello@adriankelly.net>
2016-08-17 22:53:21 +0100

    change bg and update contact functioonality

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 
   $(window).scroll(function () {
 
-    $(window).scrollTop() < 100 ? growHeading() : shrinkHeading()
+    $(window).scrollTop() < 75 ? growHeading() : shrinkHeading()
   })
 
 
@@ -22,17 +22,17 @@ $(document).ready(function() {
   //Switch
     //$('#switch').click(function() {
       function shrinkHeading() {
-        $('#main-blackout').toggleClass('main-bg-blackout');
-        if ($('#main-blackout').hasClass('main-bg-blackout')) {
-          TweenMax.to('#header-container', 0.5, { width: "18%" });
-          TweenMax.to('#header-name', 0.5, { fontSize: "2.5rem", letterSpacing: "0.3rem" });
-          TweenMax.to('#header-job-title', 0.5, { fontSize: "1.3rem", letterSpacing: "0rem", onStart: function() {
+        // $('#main-blackout').toggleClass('main-bg-blackout');
+        // if ($('#main-blackout').hasClass('main-bg-blackout')) {
+          TweenMax.to('#header-container', 0.5, { width: "25%" });
+          TweenMax.to('#header-name', 0.5, { fontSize: "3.5rem", letterSpacing: "0.3rem" });
+          TweenMax.to('#header-job-title', 0.5, { fontSize: "3.3rem", letterSpacing: "0rem", onStart: function() {
               TweenMax.to('.header-nav', 1, { opacity: 1 });
-              TweenMax.to('#contact', 1, { opacity: 1 });
+              // TweenMax.to('#contact', 1, { opacity: 1 });
               // $('a[href="#projects"]').addClass('currentLink');
             }
           });
-        }
+
       }
 
       function growHeading() {
@@ -46,13 +46,32 @@ $(document).ready(function() {
           fontSize: "3.5rem",
           onStart: function() {
             TweenMax.to('.header-nav', 0.5, { opacity: 0 });
-            TweenMax.to('#contact', 0.5, { opacity: 0 });
+            // TweenMax.to('#contact', 0.5, { opacity: 0 });
           }
         });
         // TweenMax.delayedCall(3, hideProjects);
       }
 
+      $('#contact-link').on('click', function() {
+        TweenMax.to('#contact', 0.4, { opacity: 1, display: 'block' });
+        TweenMax.to('#main-bg-cover', 0.1, { display: 'block' });
+      })
 
+      $('#main-bg-cover, #contact-close').on('click', function() {
+        TweenMax.to('#contact', 0.4, { opacity: 0, display: 'none' });
+        TweenMax.to('#main-bg-cover', 0.1, { display: 'none' });
+      })
+
+      // $('.dark').on('mouseover', function(e) {
+      //   console.log(this)
+      //   $(this).css('opacity', '0.9')
+      // })
+
+// $('.dark').mouseover(function () {
+//         $(this).css('opacity', '0.9')
+// }).mouseout(function () {
+//     $(this).css('opacity', '0')
+// });
 
     animate();
 
